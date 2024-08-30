9b748c444d5d903fa1d0a64be7f13703c90e7a77
abhishek anand <165782775+Abhishekrtp@users.noreply.github.com>
2024-08-30 19:15:10 +0530

    Add files via upload
    Creative Frontend Developer with a passion for crafting responsive, user-centric web interfaces. With a strong foundation in HTML, CSS, JavaScript, and modern frameworks, I bring designs to life with seamless functionality and aesthetic appeal. My portfolio includes projects like Amazon Clone, Skyrocket, and other dynamic web applications that showcase my ability to blend creativity with technical expertise.

---
+++ part-1-starter-files/js.js
@@ -0,0 +1,6 @@
+let about_me = document.querySelector("#about-me");
+let about = document.querySelector("#about");
+
+about.addEventListener("click", () => {
+  about_me.classList.remove("hidden");
+});
